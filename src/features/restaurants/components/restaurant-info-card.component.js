269410edaeb1bccd2526235e31f1de232380f808
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2021-03-08 17:57:10 +0100

    added a restaurant styles component,added text component,and cleaned up restaurant card info

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,49 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
+import { View } from "react-native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: white;
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.colors.ui.primary};
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
-
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
+import open from "../../../../assets/open";
+import { Text } from "../../../components/typography/text.component";
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Address,
+  Info,
+  Rating,
+  Section,
+  Open,
+  Icon,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Danbtta Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
     ],
     address = "Dantata Estate kubwa",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.ceil(rating)));
@@ -53,12 +35,25 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <RestaurantCard elevation={5}>
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
         <Info>
-          <Title>{name}</Title>
-          <Rating>
-            {ratingArray.map(() => (
-              <SvgXml xml={star} width={20} height={20} />
-            ))}
-          </Rating>
+          <Text variant="label">{name}</Text>
+          <Section>
+            <Rating>
+              {ratingArray.map(() => (
+                <SvgXml xml={star} width={20} height={20} />
+              ))}
+            </Rating>
+            {isClosedTemporarily && (
+              <Text variant="error" style={{ paddingTop: 9, marginRight: -60 }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            {isOpenNow && (
+              <Open>
+                <SvgXml xml={open} width={20} height={20} />
+              </Open>
+            )}
+            <Icon source={{ uri: icon }} />
+          </Section>
           <Address>{address}</Address>
         </Info>
       </RestaurantCard>

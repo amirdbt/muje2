921d7f9d383b2667fb8da49057644ac9a062672c
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2021-03-08 01:24:20 +0100

    Added screen folder

--- App.js
+++ App.js
@@ -1,45 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
-import { Searchbar } from "react-native-paper";
+import React from "react";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screens";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const onChangeSearch = (query) => setSearchQuery(query);
-
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <View style={styles.list}>
-          <Text>Lists</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    backgroundColor: "red",
-    padding: 15,
-  },
-  list: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "green",
-    paddingLeft: 15,
-  },
-});

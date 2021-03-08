8f9a69549fcf86b802c2fe3ddc5b8c641c2b2352
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2021-03-09 00:12:19 +0100

    added restaurant service to fetch and transform the mocked data

---
+++ src/services/restaurants/restaurants.service.js
@@ -0,0 +1,32 @@
+import { mocks } from "./mock";
+import camelize from "camelize";
+
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("Not Found");
+    }
+    resolve(mock);
+  });
+};
+
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+  return camelize(mappedResults);
+};
+
+restaurantsRequest()
+  .then(restaurantsTransform)
+  .then((transformedResults) => {
+    console.log(transformedResults);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
